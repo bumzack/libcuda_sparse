baa84fb780887d1bfc44edda80e1c4862357373a
Peter Jin <peterhaijin@gmail.com>
2016-06-02 17:12:18 -0700

    Use "cuda" crate from github.

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-  println!("cargo:rustc-flags=-L /usr/local/cuda/lib64 -l dylib=cusparse");
+  //println!("cargo:rustc-flags=-L /usr/local/cuda/lib64 -l dylib=cusparse");
+  //println!("cargo:rustc-flags=-l dylib=cusparse");
+
+  println!("cargo:rustc-flags=-l dylib=cusparse");
 }
